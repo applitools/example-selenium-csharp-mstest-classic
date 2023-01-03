ec4066d1cf17f8eecf0a73a30b49d5453686e189
Pandy Knight <andylpk.247@gmail.com>
2023-01-03 12:11:49 -0500

    Created project structure

---
+++ Applitools.Example.Tests/AcmeBankTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Applitools.Example.Tests;
+
+[TestClass]
+public class AcmeBankTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
